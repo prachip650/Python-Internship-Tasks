bf397f84f7b22bc819ae9b9f9779442e6b5588f4
Prachi Panchal <prachipanchal518@gmail.com>
2026-08-05 11:42:28 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,111 @@
+# # Task 1
+# print("Hello, World")
+# Task 2
+# name = "Prachi"
+# age = 20
+# height = 5.3
+# is_student = True
+
+# print("Name",name)
+# print("Type",type(name))
+
+# print("Age",age)
+# print("Type",type(age))
+
+# print("Height",height)
+# print("Type",type(height))
+
+# print("Student",is_student)
+# print("Type",type(isinstance))
+
+
+
+
+
+
+
+ # Task 3
+# name = input("Enter the name:")
+# age = int(input("Enter the age:"))
+# city = input("Enter the city:")
+
+# print("Hello! My name is", name+ ".")
+# print("I am",age, "years old.")
+# print("I live in",city+ ".")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Task 4
+# a = float(input("Enter first number:"))
+# b = float(input("Enter second number:"))
+
+# print("addition =",a+b)
+# print("Subtraction =",a-b)
+# print("Multiplication =",a*b)
+# print("Division =",a/b)
+# print("Modulus =",a%b)
+# print("Exponent =",a**b)
+
+
+
+
+
+
+
+ 
+# Task 5
+# Student Information 
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+course = input("Enter your course: ")
+marks = float(input("Enter your marks: "))
+
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("Marks:", marks)
+
+# Simple Calculator
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition:", num1+num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+# Age Calculator/
+current_year = int(input("Enter current year: "))
+birth_year = int(input("Enter your birth year: "))
+
+age = current_year - birth_year
+
+print("Your age is:", age)
+
+# Temperature Converter
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print("Temperature in Fahrenheit:", fahrenheit)
+
+# Area Calculator
+length = float(input("Enter length: "))
+breadth = float(input("Enter breadth: "))
+
+area = length * breadth
+
+print("Area of rectangle:", area)
+
